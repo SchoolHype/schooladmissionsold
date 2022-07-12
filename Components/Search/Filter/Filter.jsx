7fc18a4aecd8f5e531fc509786c3a4efa1164c2a
Lisha Kothari <lishakothari02@gmail.com>
2022-07-12 21:51:39 +0530

    Update Filter.jsx

--- Components/Search/Filter/Filter.jsx
+++ Components/Search/Filter/Filter.jsx
@@ -5,12 +5,30 @@ import { database } from '../../../config/firebase'
 import styles from './filter.module.css'
 
 export const Filter = () => {
-  
+
+    //useEffect(() => {
+    //    fetchData()
+    //}, [])
+  //
+    //const fetchData = () => {
+    //    const schoolInfoRef = collection(database, 'schools');
+//
+    //    const q = query(schoolInfoRef , where("living", "==", "Boarding School"));
+//
+    //    getDocs(q).then(response => {
+    //        const schoolsData = response.docs.map(doc => ({
+    //        data: doc.data(),
+    //        id: doc.id,
+    //        }))
+    //        setInfo(schoolsData)
+    //    }).catch(error => console.log(error.message))
+    //}
+
   return (
     <div className={styles.filterSection}>
         <div className={styles.filterCategory}>
             <p>Living</p>
-            <input type="radio" name="living" value="boarding"/>
+            <input type="radio" name="living" value="boarding" />
             <label htmlFor="boarding">Boarding</label><br />
             <input type="radio" name="living" value="day school" />
             <label htmlFor="day school">Day School</label><br />
